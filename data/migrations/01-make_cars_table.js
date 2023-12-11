df80eb71106de15f0482ed6ee68e24ce95a5a219
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-11 09:47:26 -0600

    initial migration complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,20 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments() // id
+    tbl.text('vin') // vin
+      .unique()
+      .notNullable()
+    tbl.text('make') // make
+      .notNullable()
+    tbl.text('model') // model
+      .notNullable()
+    tbl.decimal('mileage') // mileage
+      .notNullable()
+    tbl.text('title') // title
+    tbl.text('transmission') // tranny
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
