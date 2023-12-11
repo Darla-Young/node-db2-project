2c59f17581a285e778984ed4dc3cc4a5a76d9f47
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-11 10:35:21 -0600

    router drafted, untested

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require('./cars-middleware')
+
+router.get('/', (req, res) => {
+  Cars.getAll()
+    .then(arr => res.json(arr))
+    .catch(err => res.status(500).json(err))
+})
+
+router.get('/:id', checkCarId, (req, res) => {
+  Cars.getById(req.params.id)
+    .then(car => res.json(car))
+    .catch(err => res.status(500).json(err))
+})
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, (req, res) => {
+  Cars.create(req.body)
+    .then(car => res.json(car))
+    .catch(err => res.status(500).json(err))
+})
